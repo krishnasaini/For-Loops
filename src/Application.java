50e19070320f7da8f3477f1e894d93bf3a008d50
CHAITU SAINI <CHAITU SAINI@CHAITU>
2017-07-27 11:55:23 -0700

    For Loops

---
+++ src/Application.java
@@ -0,0 +1,12 @@
+
+public class Application {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("The Value is:" + i);
+		}
+
+		for (int i = 5; i > 1; i--) {
+			System.out.printf("The value is:%d\n", i);
+		}
+	}
+}
